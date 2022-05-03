e671130213de4c308d1fb69ac20b3e6fd9d3e7b8
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-05-02 23:11:35 -0300

    Fibonacci sequence under 30

--- TesteLogica/TesteLogica/Program.cs
+++ TesteLogica/TesteLogica/Program.cs
@@ -6,36 +6,79 @@ namespace TesteLogica
     {
         static void Main(string[] args)
         {
-            float ladoA, ladoB, ladoC;
+            int respostaOpcao = 0;
 
-            Console.WriteLine("Insira o lado A do possível Triângulo");
-            ladoA = float.Parse(Console.ReadLine());
+            Console.WriteLine("Selecione uma das opções");
+            Console.WriteLine("\n\n(0) Desenvolva um código permitindo ao usuário digitar os 3 lados de um triângulo." +
+                              " Baseado nos 3 valores digitados, deverá ser exibido se o triangulo é “inválido”," +
+                              " “equilátero”, “isósceles” ou “escaleno”.");
 
-            Console.WriteLine("Insira o lado B do possível Triângulo");
-            ladoB = float.Parse(Console.ReadLine());
+            Console.WriteLine("\n\n(1) Desenvolva uma aplicação para exibir na tela, com o uso de " +
+                              "recursividade, " +"os 30 primeiros resultados da “Sequência de Fibonacci”");
 
-            Console.WriteLine("Insira o lado C do possível Triângulo");
-            ladoC = float.Parse(Console.ReadLine());
+            Console.WriteLine("\n\n\n");
+            respostaOpcao = Int16.Parse(Console.ReadLine());
 
-            if (ladoC > ladoA + ladoB || ladoA > ladoB + ladoC || ladoB > ladoA + ladoC)
+            if (respostaOpcao == 0) 
             {
-                Console.WriteLine("Dados não formam um triângulo perfeito");
+                float ladoA, ladoB, ladoC;
+
+                Console.WriteLine("Insira o lado A do possível Triângulo");
+                ladoA = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Insira o lado B do possível Triângulo");
+                ladoB = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Insira o lado C do possível Triângulo");
+                ladoC = float.Parse(Console.ReadLine());
+
+                if (ladoC > ladoA + ladoB || ladoA > ladoB + ladoC || ladoB > ladoA + ladoC)
+                {
+                    Console.WriteLine("Dados não formam um triângulo perfeito");
+                }
+                else if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC)
+                {
+                    Console.WriteLine("Este é um Esacleno!");
+                }
+                else if (ladoA == ladoB && ladoA == ladoC)
+                {
+                    Console.WriteLine("Este é um Equilatero!");
+                }
+                else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+                {
+                    Console.WriteLine("Este é um Isosceles!");
+                }
+                else
+                {
+                    Console.WriteLine("Inválido!");
+                }
+
+                Console.ReadLine();
             }
-            else if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC)
+            else if(respostaOpcao == 1)
             {
-                Console.WriteLine("Este é um Esacleno!");
-            }
-            else if (ladoA == ladoB && ladoA == ladoC)
-            {
-                Console.WriteLine("Este é um Equilatero!");
-            }
-            else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
-            {
-                Console.WriteLine("Este é um Isosceles!");
-            }
-            else
-            {
-                Console.WriteLine("Inválido!");
+                int tamanhoSequencia = 30;
+
+                Console.WriteLine("\n\n\n");
+                Console.WriteLine($"\n\nRESULTADO DE {tamanhoSequencia} primeiros números:");
+
+                for (int a = 0; a <= tamanhoSequencia; a++)
+                {
+                    Console.Write(" " + Fibonacci(a + 1));
+                }
+                Console.ReadKey();
+
+                static int Fibonacci(int n)
+                {
+                    if (n == 1 || n == 0)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return Fibonacci(n - 1) + Fibonacci(n - 2);
+                    }
+                }
             }
 
             Console.ReadLine();
