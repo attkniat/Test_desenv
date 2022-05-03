c2b9d81e7981b491a799b834a3ac7b838dbe8841
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-05-03 11:39:32 -0300

    Adding a brand new option to the main menu

--- TesteLogica/TesteLogica.Engine/TesteLogica.Workers/TesteLogica.Menus/MenuInicialOpcoes.cs
+++ TesteLogica/TesteLogica.Engine/TesteLogica.Workers/TesteLogica.Menus/MenuInicialOpcoes.cs
@@ -14,6 +14,10 @@ namespace TesteLogica
             Console.WriteLine("\n\n(1) Desenvolva uma aplicação para exibir na tela, com o uso de " +
                               "recursividade, " + "os 30 primeiros resultados da “Sequência de Fibonacci”");
 
+            Console.WriteLine("\n\n(2) Considerando o diagrama de classes abaixo, temos um objeto “List<Casa> casas” " +
+                              "que pode conter N elementos. Exiba para o usuário o “Endereço”, “Metragem” e a quantidade " +
+                              "de “Comodos” apenas das casas que possuam pelo menos 1 comodo com numero par de janelas.");
+
             Console.WriteLine("\n\n\n");
             var respostaOpcao = Int16.Parse(Console.ReadLine());
 
