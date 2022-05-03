a6480b274a5e06e71fc15d74002392eab16599dd
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-05-02 22:51:50 -0300

    Validating input data to tell if there is a triangle

--- TesteLogica/TesteLogica/Program.cs
+++ TesteLogica/TesteLogica/Program.cs
@@ -6,7 +6,39 @@ namespace TesteLogica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float ladoA, ladoB, ladoC;
+
+            Console.WriteLine("Insira o lado A do possível Triângulo");
+            ladoA = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o lado B do possível Triângulo");
+            ladoB = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o lado C do possível Triângulo");
+            ladoC = float.Parse(Console.ReadLine());
+
+            if (ladoC > ladoA + ladoB || ladoA > ladoB + ladoC || ladoB > ladoA + ladoC)
+            {
+                Console.WriteLine("Dados não formam um triângulo perfeito");
+            }
+            else if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC)
+            {
+                Console.WriteLine("Este é um Esacleno!");
+            }
+            else if (ladoA == ladoB && ladoA == ladoC)
+            {
+                Console.WriteLine("Este é um Equilatero!");
+            }
+            else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+            {
+                Console.WriteLine("Este é um Isosceles!");
+            }
+            else
+            {
+                Console.WriteLine("Inválido!");
+            }
+
+            Console.ReadLine();
         }
     }
 }
